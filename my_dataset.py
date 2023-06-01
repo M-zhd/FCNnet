18a6d8b33392ea8268ce67438bcebc0e5cdf198f
M-zhd <1552809876@qq.com>
2023-06-01 14:21:03 +0800

    第一次提交

---
+++ my_dataset.py
@@ -0,0 +1,74 @@
+import os
+
+import torch.utils.data as data
+from PIL import Image
+
+
+# 创建一个符合自己要求的dataset的方法
+# 获取训练集或验证集的图片和标签路径
+# 打开数据并根据传入的transform对数据进行数据预处理
+class VOCSegmentation(data.Dataset):
+    def __init__(self, voc_root, year="2012", transforms=None, txt_name: str = "train.txt"):
+        super(VOCSegmentation, self).__init__()
+        assert year in ["2007", "2012"], "year must be in ['2007', '2012']"
+        root = os.path.join(voc_root, "VOCdevkit", f"VOC{year}")
+        assert os.path.exists(root), "path '{}' does not exist.".format(root)
+        image_dir = os.path.join(root, 'JPEGImages')
+        mask_dir = os.path.join(root, 'SegmentationClass')
+
+        txt_path = os.path.join(root, "ImageSets", "Segmentation", txt_name)
+        assert os.path.exists(txt_path), "file '{}' does not exist.".format(txt_path)
+        # 读取训练集图片名字
+        with open(os.path.join(txt_path), "r") as f:
+            file_names = [x.strip() for x in f.readlines() if len(x.strip()) > 0]
+
+        # 获取训练集图片和标签的路径
+        self.images = [os.path.join(image_dir, x + ".jpg") for x in file_names]
+        self.masks = [os.path.join(mask_dir, x + ".png") for x in file_names]
+        assert (len(self.images) == len(self.masks))
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is the image segmentation.
+        """
+        # 打开图像和标签
+        img = Image.open(self.images[index]).convert('RGB')
+        target = Image.open(self.masks[index])
+
+        # 数据预处理
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        # 返回图像和标签
+        return img, target
+
+    def __len__(self):
+        return len(self.images)
+
+    # 自定义批次整理函数 默认参数就是getitem函数返回的数据项的batch形成的列表
+    @staticmethod
+    def collate_fn(batch):
+        images, targets = list(zip(*batch))
+        batched_imgs = cat_list(images, fill_value=0)
+        batched_targets = cat_list(targets, fill_value=255)
+        return batched_imgs, batched_targets
+
+
+def cat_list(images, fill_value=0):
+    # 计算该batch数据中，channel, h, w的最大值
+    max_size = tuple(max(s) for s in zip(*[img.shape for img in images]))
+    batch_shape = (len(images),) + max_size
+    batched_imgs = images[0].new(*batch_shape).fill_(fill_value)
+    for img, pad_img in zip(images, batched_imgs):
+        pad_img[..., :img.shape[-2], :img.shape[-1]].copy_(img)
+    return batched_imgs
+
+
+# dataset = VOCSegmentation(voc_root="/data/", transforms=get_transform(train=True))
+# d1 = dataset[0]
+# print(d1)
---
+++ train.py
@@ -0,0 +1,232 @@
+import os
+import time
+import datetime
+
+import torch
+
+from src import fcn_resnet50
+from train_utils import train_one_epoch, evaluate, create_lr_scheduler
+from my_dataset import VOCSegmentation
+import transforms as T
+
+
+# train的transform
+class SegmentationPresetTrain:
+    def __init__(self, base_size, crop_size, hflip_prob=0.5, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
+        min_size = int(0.5 * base_size)
+        max_size = int(2.0 * base_size)
+
+        trans = [T.RandomResize(min_size, max_size)]
+        if hflip_prob > 0:
+            trans.append(T.RandomHorizontalFlip(hflip_prob))
+        # append将整个对象加在列表末尾
+        # extend将新对象中的元素逐一加在列表的末尾
+        trans.extend([
+            T.RandomCrop(crop_size),
+            T.ToTensor(),
+            T.Normalize(mean=mean, std=std),
+        ])
+        self.transforms = T.Compose(trans)
+
+    def __call__(self, img, target):
+        return self.transforms(img, target)
+
+
+# validation的transform
+class SegmentationPresetEval:
+    def __init__(self, base_size, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
+        self.transforms = T.Compose([
+            T.RandomResize(base_size, base_size),
+            T.ToTensor(),
+            T.Normalize(mean=mean, std=std),
+        ])
+
+    def __call__(self, img, target):
+        return self.transforms(img, target)
+
+
+# 设置不同的transform
+def get_transform(train):
+    base_size = 520
+    crop_size = 480
+
+    return SegmentationPresetTrain(base_size, crop_size) if train else SegmentationPresetEval(base_size)
+
+
+def create_model(aux, num_classes, pretrain=True):
+    model = fcn_resnet50(aux=aux, num_classes=num_classes)
+
+    # 加载预训练权重
+    if pretrain:
+        weights_dict = torch.load("./fcn_resnet50_coco.pth", map_location='cpu')
+
+        if num_classes != 21:
+            # 官方提供的预训练权重是21类(包括背景)
+            # 如果训练自己的数据集，将和类别相关的权重删除，防止权重shape不一致报错
+            for k in list(weights_dict.keys()):
+                if "classifier.4" in k:
+                    del weights_dict[k]
+
+        # 自己的模型和预训练权重的参数不完全对应，所以需要strict=False
+        missing_keys, unexpected_keys = model.load_state_dict(weights_dict, strict=False)
+        if len(missing_keys) != 0 or len(unexpected_keys) != 0:
+            print("missing_keys: ", missing_keys)
+            print("unexpected_keys: ", unexpected_keys)
+
+    return model
+
+
+def main(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+    print("using {} device.".format(device))
+    batch_size = args.batch_size
+    # segmentation nun_classes + background
+    num_classes = args.num_classes + 1
+
+    # 用来保存训练以及验证过程中信息
+    results_file = "results{}.txt".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+    # VOCdevkit -> VOC2012 -> ImageSets -> Segmentation -> train.txt
+    train_dataset = VOCSegmentation(args.data_path,
+                                    year="2012",
+                                    transforms=get_transform(train=True),
+                                    txt_name="train.txt")
+
+    # VOCdevkit -> VOC2012 -> ImageSets -> Segmentation -> val.txt
+    val_dataset = VOCSegmentation(args.data_path,
+                                  year="2012",
+                                  transforms=get_transform(train=False),
+                                  txt_name="val.txt")
+
+    num_workers = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])
+    # pin_memory：拷贝数据到 CUDA Pinned Memory
+    # collate_fn ：手动将抽取出的样本堆叠起来。Torch 中可以进行自定义。
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size,
+                                               num_workers=num_workers,
+                                               shuffle=True,
+                                               pin_memory=True,
+                                               collate_fn=train_dataset.collate_fn)
+
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=1,
+                                             num_workers=num_workers,
+                                             pin_memory=True,
+                                             collate_fn=val_dataset.collate_fn)
+
+    model = create_model(aux=args.aux, num_classes=num_classes)
+    model.to(device)
+
+    # 获取模型参数
+    params_to_optimize = [
+        {"params": [p for p in model.backbone.parameters() if p.requires_grad]},
+        {"params": [p for p in model.classifier.parameters() if p.requires_grad]}
+    ]
+
+    if args.aux:
+        params = [p for p in model.aux_classifier.parameters() if p.requires_grad]
+        params_to_optimize.append({"params": params, "lr": args.lr * 10})
+
+    # momentum：SGD通常来说下降速度比较快，但却容易造成另一个问题，就是更新过程不稳定，容易出现震荡。
+    # 加入“惯性”的影响，就是在更新下降方向的时候不仅要考虑到当前的方向，也要考虑到上一次的更新方向，
+    # 两者加权，某些情况下可以避免震荡，摆脱局部凹域的束缚，进入全局凹域。
+    # weight_decay：权重衰减 防止过拟合
+    optimizer = torch.optim.SGD(
+        params_to_optimize,
+        lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay
+    )
+
+    # 是否使用自动混合精度训练 可以短训练时间，降低存储需求
+    scaler = torch.cuda.amp.GradScaler() if args.amp else None
+
+    # 创建学习率更新策略，这里是每个step更新一次(不是每个epoch)
+    lr_scheduler = create_lr_scheduler(optimizer, len(train_loader), args.epochs, warmup=True)
+
+    if args.resume:
+        checkpoint = torch.load(args.resume, map_location='cpu')
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+        args.start_epoch = checkpoint['epoch'] + 1
+        if args.amp:
+            scaler.load_state_dict(checkpoint["scaler"])
+
+    start_time = time.time()
+    for epoch in range(args.start_epoch, args.epochs):
+        mean_loss, lr = train_one_epoch(model, optimizer, train_loader, device, epoch,
+                                        lr_scheduler=lr_scheduler, print_freq=args.print_freq, scaler=scaler)
+
+        confmat = evaluate(model, val_loader, device=device, num_classes=num_classes)
+        val_info = str(confmat)
+        print(val_info)
+        # write into txt
+        with open(results_file, "a") as f:
+            # 记录每个epoch对应的train_loss、lr以及验证集各指标
+            train_info = f"[epoch: {epoch}]\n" \
+                         f"train_loss: {mean_loss:.4f}\n" \
+                         f"lr: {lr:.6f}\n"
+            f.write(train_info + val_info + "\n\n")
+
+        save_file = {"model": model.state_dict(),
+                     "optimizer": optimizer.state_dict(),
+                     "lr_scheduler": lr_scheduler.state_dict(),
+                     "epoch": epoch,
+                     "args": args}
+        if args.amp:
+            save_file["scaler"] = scaler.state_dict()
+        torch.save(save_file, "./save_weights/model_{}.pth".format(epoch))
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print("training time {}".format(total_time_str))
+
+
+# 获取一系列参数
+def parse_args():
+    # 获取命令行参数，让程序执行
+    import argparse
+    # 对参数进行描述
+    parser = argparse.ArgumentParser(description="pytorch fcn training")
+
+    # 开始传入参数
+    # "--data-path"为参数名字 参数名前加--类似关键字参数
+    # default为参数默认值 help为参数提示信息
+    parser.add_argument("--data-path", default="./data", help="VOCdevkit root")
+    # type为参数数据类型
+    parser.add_argument("--num-classes", default=20, type=int)
+    parser.add_argument("--aux", default=True, type=bool, help="auxilier loss")
+    parser.add_argument("--device", default="cuda", help="training device")
+    parser.add_argument("-b", "--batch-size", default=4, type=int)
+    # metavar用于丰富help的帮助文档
+    parser.add_argument("--epochs", default=30, type=int, metavar="N",
+                        help="number of total epochs to train")
+
+    parser.add_argument('--lr', default=0.0001, type=float, help='initial learning rate')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    # dest为使用weight_decay代替原有参数名称--wd
+    # dest不能与meataver同时使用
+    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)',
+                        dest='weight_decay')
+    parser.add_argument('--print-freq', default=10, type=int, help='print frequency')
+    parser.add_argument('--resume', default='', help='resume from checkpoint')
+    parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+    # Mixed precision training parameters
+    parser.add_argument("--amp", default=False, type=bool,
+                        help="Use torch.cuda.amp for mixed precision training")
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    # 判断文件夹是否存在 不存在就新建一个
+    if not os.path.exists("./save_weights"):
+        os.mkdir("./save_weights")
+
+    main(args)
